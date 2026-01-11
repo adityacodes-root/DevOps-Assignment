5826481a114bb0646efac4644c0e5456e510c452
aditya dubey <aditya.d2676@gmail.com>
2026-01-11 11:08:44 +0530

    Fix: Restore alb_sg security group(removed previously)

--- terraform/aws/security.tf
+++ terraform/aws/security.tf
@@ -1,20 +1,40 @@
-resource "aws_security_group" "ecs_tasks_sg" {
-  name        = "devops-ecs-tasks-sg"
-  description = "Allow inbound traffic directly"
+resource "aws_security_group" "alb_sg" {
+  name        = "devops-alb-sg"
+  description = "Allow inbound HTTP traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_tasks_sg" {
+  name        = "devops-ecs-tasks-sg"
+  description = "Allow inbound traffic from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
